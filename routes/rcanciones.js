63f89e006d27580e556a7c36e462e7c7003c3a89
UO269509 <uo269509@uniovi.es>
2021-04-07 17:03:17 +0200

    SDI-2021-405-9.2-Redirección de peticiones

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -65,7 +65,7 @@ module.exports = function(app, swig, gestorBD) {
                                     if (err) {
                                         res.send("Error al subir el audio");
                                     } else {
-                                        res.send("Agregada id: "+ id);
+                                        res.redirect("/publicaciones");
                                     }
                                 });
                             }
@@ -162,7 +162,7 @@ module.exports = function(app, swig, gestorBD) {
                     if( result == null){
                         res.send("Error en la modificación");
                     } else {
-                        res.send("Modificado");
+                        res.redirect("/publicaciones");
                     }
                 });
             }
